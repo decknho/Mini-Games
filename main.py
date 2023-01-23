a872cbdb881ee13dc747216dba6e237438ea55b2
Dereck Eder <decknho26@gmail.com>
2023-01-23 07:38:53 -0300

    o menu dos mini games

---
+++ main.py
@@ -0,0 +1,15 @@
+from jogos import *
+while True:
+      print('[ 1 ] - Pedra, Papel ou Tesoura\n'
+            '[ 2 ] - Par ou impar\n'
+            '[ 3 ] - Adivinhe um numero\n'
+            '[ 4 ] - Finalizar programa')
+      minigame = int(input('Qual mini-game você deseja jogar? '))
+      if minigame == 1:
+            ppt()
+      if minigame == 2:
+            par_imp()
+      if minigame == 3:
+            adivinhe()
+      if minigame == 4:
+            break
